42f947b8c65cf5d80c048aa030004181cd48cc54
Lukasz Winowicz <winowiczlukasz@gmail.com>
2019-06-09 19:40:14 +0200

    init project

---
+++ iAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAmRich
+//
+//  Created by Łukasz Winowicz on 09/06/2019.
+//  Copyright © 2019 Łukasz Winowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
